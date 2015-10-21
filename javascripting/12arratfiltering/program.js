0fe5713d7142e020ab1bfe2af24e22bd385b6b31
Aleksandr Kravtsov <sabahtalateh@gmail.com>
2015-10-21 19:51:31 +0700

    wip

---
+++ javascripting/12arratfiltering/program.js
@@ -0,0 +1,13 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+var isEven = function(n) {
+    return n % 2 === 0
+}
+
+var even = numbers.filter(function(i) {
+    return isEven(i)
+})
+
+
+
+console.log(even)
